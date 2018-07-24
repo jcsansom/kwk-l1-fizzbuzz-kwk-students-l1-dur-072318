86d1a4af268529e5f4a710969294a63af755bff3
jcsansom <jayceesansom@gmail.com>
2018-07-24 18:08:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def soup(x)
   if x%4==0 && x%11==0 
-    puts "Chicken Noodle"
+    puts "Chicken Noodle Soup"
     elsif x%4==0 
-    puts "Chicken"
+    puts "Chicken Soup"
     elsif x%11==0 
-    puts "Noodle"
+    puts "Noodle Soup"
   else 
     puts "I hate soup"
   end
